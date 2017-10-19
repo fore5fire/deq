9429acddb8e23aeca38c76fd593cb165a8330d98
Landon Smith <ls@katchecode.com>
2017-10-18 20:55:40 -0600

    Built CRUD operations for tasks

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import { Server } from './server';
 import { Health } from './health';
 import { spawn } from 'child_process';
+import mongoose from 'mongoose';
 
-const config = {
-  port: process.env.PORT || 3000,
-  healthPort: process.env.HEALTH_PORT || 3333
-};
+const { PORT = 3000, HEALTH_PORT = 3333 } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/task-service', { useMongoClient: true });
 
 const health = new Health({
   alive: () => true,
-  ready: () => true,
-}).listen(config.healthPort);
+  ready: () => mongoose.connection.readyState === 1,
+}).listen(HEALTH_PORT);
 
 
 const app = new Server();
 
-const server = app.listen(config.port);
-console.log(`Listening on port ${config.port}`);
+const server = app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
 
 if (process.argv[2]) {
   console.log('Running comand: ' + process.argv.slice(2).join(' '));
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,17 +1,41 @@
 
 import { makeExecutableSchema } from 'graphql-tools';
 import typeDefs from './schema.graphql';
-import Greeting from './greeting';
+import { Task, MutableTask, MutableAssetTask } from './task';
+import { GraphQLDate } from 'graphql-iso-date';
+import { GraphQLUrl } from 'graphql-url';
 
 const resolvers = {
   Query: {
-    greeting(obj, { name = "Jimmy" }, context, info) {
-      return new Greeting(name);
+    async task(obj, { id }) {
+      return await Task.findById(id);
+    },
+    async tasks(obj, { filter }) {
+      return await Task.find(filter).exec();
     }
   },
-  // Mutation: {
-  //
-  // }
+  Mutation: {
+    async createAssetTask(obj, { input }) {
+      const task = new MutableAssetTask(input);
+      await task.save();
+      return task;
+    },
+    async task(obj, { id }) {
+      return await MutableTask.findById(id);
+    }
+  },
+  Task: {
+    __resolveType(obj, context, info) {
+      return info.schema.getType(obj.type);
+    }
+  },
+  MutableTask: {
+    __resolveType(obj, context, info) {
+      return info.schema.getType(obj.type);
+    }
+  },
+  Date: GraphQLDate,
+  Url: GraphQLUrl
 };
 
 
