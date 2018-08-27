19a48929912ec36a3f7120aae1829f1601864c2e
Landon Smith <ls@katchecode.com>
2018-08-27 06:58:00 -0400

    work on cli

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
 	"math/rand"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"gitlab.com/katcheCode/deqd/api/v1/deq"
@@ -13,8 +16,19 @@ import (
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	host := flag.String("host", "localhost:3000", "specify deq host and port. defaults to localhost:8080")
+	channel := flag.String("c", strconv.FormatInt(int64(rand.Int()), 16), "specify channel. defaults to random")
+	follow := flag.Bool("f", false, "continue streaming when idling. defaults to false")
+	eventType := flag.String("t", "", "event type to print. defaults to all types")
+
 	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	cmd := flag.Arg(0)
 	switch cmd {
@@ -25,11 +39,9 @@ func main() {
 		}
 		deqc := deq.NewDEQClient(conn)
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-		defer cancel()
-
-		stream, err := deqc.StreamEvents(ctx, &deq.StreamEventsRequest{
-			Channel: strconv.FormatInt(int64(rand.Int()), 16),
+		stream, err := deqc.StreamEvents(context.Background(), &deq.StreamEventsRequest{
+			Channel: *channel,
+			Follow:  *follow,
 		})
 		if err != nil {
 			log.Fatalf("stream: %v", err)
@@ -37,10 +49,15 @@ func main() {
 
 		for {
 			e, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
 			if err != nil {
 				log.Fatalf("recieve message: %v", err)
 			}
-			log.Printf("id: %v, type: %s", e.Id, e.Payload.TypeUrl)
+			if *eventType == "" || strings.TrimPrefix(e.Payload.TypeUrl, "type.googleapis.com/") == *eventType {
+				log.Printf("id: %v, type: %s", e.Id, e.Payload.TypeUrl)
+			}
 		}
 
 	case "help":
