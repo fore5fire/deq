e9fce5a0cbaf7c6722b82a3be3407cb0a1296892
Landon Smith <ls@katchecode.com>
2019-01-12 18:47:35 -0700

    cap requeue count to 12 for Ack with REQUEUE_EXPONENTIAL

--- pkg/grpc/eventstore/eventstore.go
+++ pkg/grpc/eventstore/eventstore.go
@@ -192,7 +192,8 @@ func (s *Server) Ack(ctx context.Context, in *pb.AckRequest) (*pb.AckResponse, e
 		case pb.AckCode_REQUEUE_LINEAR:
 			delay = baseTime * time.Duration(e.RequeueCount+1)
 		case pb.AckCode_REQUEUE_EXPONENTIAL:
-			delay = time.Duration(math.Pow(2, float64(e.RequeueCount))) * baseTime
+			cappedRequeue := math.Min(float64(e.RequeueCount), 12)
+			delay = time.Duration(math.Pow(2, cappedRequeue)) * baseTime
 		}
 
 		if delay < 0 || delay > time.Hour {
